9579e8f18f55d35329f548c75c6afa87c04f8b96
Anush Mattapalli <matanatr96@gmail.com>
2017-09-12 12:43:53 -0400

    defined new hyperparameters and conducted more tests towards model saving

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (d
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
+tf.flags.DEFINE_float("model_version", 1.0, "Current working model version")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
@@ -34,6 +35,8 @@ tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (d
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+tf.flags.Define_integer("max_iterations", 10000, "Max number of training iterations")
+
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -144,6 +147,8 @@ with tf.Graph().as_default():
             x_batch, y_batch = zip(*batch)
             train_step(x_batch, y_batch)
             current_step = tf.train.global_step(sess, global_step)
+            if current_step == FLAGS.max_iterations:
+                break
 
         export_path_base = sys.argv[-1]
         export_path = os.path.join(
