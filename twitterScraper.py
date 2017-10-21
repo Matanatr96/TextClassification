9333d6eee7aa1a841d649231d2af3b8881321f61
Anush Mattapalli <matanatr96@gmail.com>
2017-10-20 22:57:21 -0400

    fixed function call :

--- twitterScraper.py
+++ twitterScraper.py
@@ -1,14 +1,15 @@
 # from twitterscraper import query_tweets
 import eval
 
+
 # for tweet in query_tweets("Trump", 10)[:10]:
 # 	print(tweet.text)
 
 
-
 def runEval(evaluation):
-	eval.tempPrint(evaluation)
+    eval.runAnalysis()
+    eval.tempPrint(evaluation)
 
 
 if __name__ == "__main__":
-	runEval('twitterScraper.py')
+    runEval('twitterScraper.py')
