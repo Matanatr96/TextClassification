de2bfeb9cf23b02f21bab47136dfcaeff4b2ca8d
Anush Mattapalli <matanatr96@gmail.com>
2017-10-20 22:55:38 -0400

    added functions to run analysis on command

--- twitterScraper.py
+++ twitterScraper.py
@@ -1,5 +1,14 @@
-from twitterscraper import query_tweets
+# from twitterscraper import query_tweets
+import eval
 
-for tweet in query_tweets("Trump", 10)[:10]:
-	print(tweet.text)
+# for tweet in query_tweets("Trump", 10)[:10]:
+# 	print(tweet.text)
 
+
+
+def runEval(evaluation):
+	eval.tempPrint(evaluation)
+
+
+if __name__ == "__main__":
+	runEval('twitterScraper.py')
